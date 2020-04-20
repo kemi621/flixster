eee7f9397058ff987ab6859cd18628e6f4190153
kemi621 <miriamakande@yahoo.com>
2020-04-19 18:29:34 -0700

    Added Superhero Movies Collection View

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by user168675 on 4/19/20.
+//  Copyright © 2020 miriamakande. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
